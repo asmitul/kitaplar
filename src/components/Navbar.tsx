e9b10a4e0b9729ef60b6b2420402553a9521e135
asmitul <asmitul@users.noreply.github.com>
2024-02-03 12:44:26 +0300

    v0.0.19

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
               {/* TODO MOBILE NAV */}
-              <div className="ml-4 flex lg:ml-0">
+              <div className="mr-4 flex lg:ml-0">
                 <Link href="/">
                   {/* add logo from src/components/Icons.tsx */}
                   <Icons.logo className="h-10 w-10" />
